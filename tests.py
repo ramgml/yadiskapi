641f699d2c7ffa4ae92f8aa90157583988942064
kovalevvjatcheslav <kovalevvjatcheslav@gmail.com>
2018-05-12 22:13:23 +0500

    Добавил еще несколько функций.

--- tests.py
+++ tests.py
@@ -7,14 +7,49 @@ class TestDiskApi(TestCase):
         self.disk = Disk(token=TOKEN)
 
     def test_get_info(self):
-        info = self.disk.get_info()
-        self.assertEqual(info.get('error'), None,
-                         'Ошибка при вызове функции get_info: {}'.format(info.get('message')))
+        response = self.disk.get_info()
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции get_info: {}'.format(response.get('message')))
 
     def test_get_resources_metainfo(self):
-        info = self.disk.get_resources_metainfo('app:/')
-        self.assertEqual(info.get('error'), None,
-                         'Ошибка при вызове функции get_resources_metainfo: {}'.format(info.get('message')))
+        response = self.disk.get_resources_metainfo('app:/')
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции get_resources_metainfo: {}'.format(response.get('message')))
+
+    def test_create_and_delete_dir(self):
+        response = self.disk.create_dir('app:/test')
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции create_dir: {}'.format(response.get('message')))
+        response = self.disk.delete_dir('app:/test')
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции delete_dir: {}'.format(response.get('message')))
+
+    def test_change_resources_metainfo(self):
+        self.disk.create_dir('app:/test')
+        response = self.disk.change_resources_metainfo('app:/test', {'test1': '1', 'test2': '2'})
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции change_resources_metainfo: {}'.format(response.get('message')))
+        self.disk.delete_dir('app:/test')
+
+    def test_copy_resources(self):
+        self.disk.create_dir('app:/test')
+        response = self.disk.copy_resources('app:/test', 'app:/test1')
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции copy_resources: {}'.format(response.get('message')))
+        self.disk.delete_dir('app:/test')
+        self.disk.delete_dir('app:/test1')
+
+    def test_download(self):
+        self.disk.create_dir('app:/test')
+        response = self.disk.download('app:/test')
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции download: {}'.format(response.get('message')))
+        self.disk.delete_dir('app:/test')
+
+    def test_get_files(self):
+        response = self.disk.get_files(media_type='image', offset=0, preview_size='100x100', sort='resource_id')
+        self.assertEqual(response.get('error'), None,
+                         'Ошибка при вызове функции get_files: {}'.format(response.get('message')))
 
 if __name__ == '__main__':
     main()
--- yadiskapi.py
+++ yadiskapi.py
@@ -19,13 +19,98 @@ class Disk(object):
         self.api_url = 'https://cloud-api.yandex.net/{}/'.format(version_api)
 
     def get_info(self):
+        """Возвращает информацию о диске"""
         response = requests.get(url='{}disk'.format(self.api_url), headers=self.headers)
         if response.status_code != 200:
             raise DiskException(response.json())
         return response.json()
 
     def get_resources_metainfo(self, path):
+        """Возвращает мета-информацию о ресурсе"""
         response = requests.get(url='{}disk/resources'.format(self.api_url), params={'path': path}, headers=self.headers)
+        if response.status_code != 200:
+            raise DiskException(response.json())
+        return response.json()
+
+    def create_dir(self, path):
+        """Создает папку"""
+        response =requests.put(url='{}disk/resources'.format(self.api_url), params={'path': path}, headers=self.headers)
+        if response.status_code != 201:
+            raise DiskException(response.json())
+        return response.json()
+
+    def delete_dir(self, path):
+        """Удаляет папку"""
+        response = requests.delete(url='{}disk/resources'.format(self.api_url), params={'path': path},
+                                   headers=self.headers)
+        if response.status_code not in [202, 204]:
+            raise DiskException(response.json())
+        return {'success': True}
+
+    def change_resources_metainfo(self, path, custom_properties):
+        """Изменяет метаинформацию о ресурсе
+        custom_properties - словарь, где ключ - имя поля в виде строки"""
+        response = requests.patch(url='{}disk/resources'.format(self.api_url),
+                                 params={'path': path,'custom_properties': custom_properties}, headers=self.headers)
+        if response.status_code != 200:
+            raise DiskException(response.json())
+        return response.json()
+
+    def copy_resources(self, from_resource, path_to_copy, force_async=False, owerwrite=False):
+        """Копирует ресурс"""
+        response = requests.post(url='{}disk/resources/copy'.format(self.api_url),
+                                 params={'from': from_resource, 'path': path_to_copy, 'force_async': force_async,
+                                         'owerwrite': owerwrite},
+                                 headers=self.headers)
+        if response.status_code not in [201, 202]:
+            raise DiskException(response.json())
+        return response.json()
+
+    def download(self, path):
+        """Возвращает ссылку на скачивание файла или папки"""
+        response = requests.get(url='{}disk/resources/download'.format(self.api_url),
+                                params={'path': path}, headers= self.headers)
+        if response.status_code != 200:
+            raise DiskException(response.json())
+        return response.json()
+
+    def get_files(self, media_type, offset, preview_size, sort, limit=20, preview_crop=False):
+        """Возвращает список файлов
+        media_type принимает следующие значения
+            audio — аудио-файлы.
+            backup — файлы резервных и временных копий.
+            book — электронные книги.
+            compressed — сжатые и архивированные файлы.
+            data — файлы с базами данных.
+            development — файлы с кодом (C++, Java, XML и т. п.), а также служебные файлы IDE.
+            diskimage — образы носителей информации и сопутствующие файлы (например, ISO и CUE).
+            document — документы офисных форматов (Word, OpenOffice и т. п.).
+            encoded — зашифрованные файлы.
+            executable — исполняемые файлы.
+            flash — файлы с флэш-видео или анимацией.
+            font — файлы шрифтов.
+            image — изображения.
+            settings — файлы настроек для различных программ.
+            spreadsheet — файлы офисных таблиц (Excel, Numbers, Lotus).
+            text — текстовые файлы.
+            unknown — неизвестный тип.
+            video — видео-файлы.
+            web — различные файлы, используемые браузерами и сайтами (CSS, сертификаты, файлы закладок).
+        sort принимает следующие значения:
+            resource_id — Идентификатор ресурса,
+            size — Размер файла,
+            media_type — Определённый Диском тип файла,
+            sha256 — SHA256-хэш,
+            revision — Ревизия Диска в которой этот ресурс был изменён последний раз,
+            path — Путь к ресурсу,
+            md5 — MD5-хэш,
+            name — Имя,
+            created — Дата создания,
+            modified — Дата изменения"""
+        response = requests.get(url='{}disk/resources/files'.format(self.api_url),
+                                params={'media_type': media_type, 'offset': offset, 'preview_size': preview_size,
+                                        'sort': sort, 'limit': limit, 'preview_crop': preview_crop},
+                                headers= self.headers)
         if response.status_code != 200:
             raise DiskException(response.json())
         return response.json()
